b809bc6632ae381bcfdfb0c7ae51872ad8ba2cb9
eemeligustafsson <eemeli.gustafsson@hotmail.com>
2022-11-23 15:54:33 +0200

    added validators

--- index.js
+++ index.js
@@ -31,36 +31,55 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    response.status(404).end();
-  } else {
-    response.json(person);
-  }
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+        response.json(person.toJSON())
+    } else {
+        response.status(404).end()
+    }
+  })
 });
+
 const generateId = () => {
   return Math.floor(Math.random() * 100000);
 };
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 });
-app.post("/api/persons", (request, response) => {
+
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   const person = new Person({
     name: body.name,
     number: body.number
   })
-  person.save().then(savedPerson => {
+  person.save()
+  .then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 });
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
 
 app.get("/info", (request, response) => {
-  const len = persons.length;
+  const len = Person.length
   const currentDate = new Date().toLocaleString();
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   response.send(`
@@ -71,6 +90,17 @@ app.get("/info", (request, response) => {
         <p>${currentDate} (${timeZone})</p>
     </div>`);
 });
+const errorHandler = (error, request, response, next) =>{
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'bad format for id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    } 
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT =  process.env.PORT || 3002;
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,28 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URL;
 
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+console.log("connecting to", url);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-  })
+const numberValidator = [
+  {
+    validator: (number) => {
+      if ((number[2] === "-" || number[3] === "-") && number.length >= 8) {
+        return true;
+      }
+      return false;
+    },
+    msg: "number must be at leats 8 digits long & be in form like xx-xxxxxxx or xxx-xxxxxxxx",
+  },
+  {
+    validator: (number) => {
+        return /^\d{2,3}-\d+$/.test(number);
+    },
+    msg: "number must consist of only numeric values"
+  },
+];
 
-  personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-  })
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    validate: numberValidator,
+    required: true,
+  },
+});
 
-  module.exports = mongoose.model("Person", personSchema);
-  
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
